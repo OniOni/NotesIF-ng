013d610c23f072d893dea7375663bbf951201c63
mat <sabourin.mathieu@free.fr>
2012-02-14 11:08:17 +0100

    Added migration

--- notesif/settings.py
+++ notesif/settings.py
@@ -112,7 +112,8 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'studentapp'
+    'studentapp',
+    'south'
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
