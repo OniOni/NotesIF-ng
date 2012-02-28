98d28f126dd89fd8552203ccdf9e85db35a46412
root <root@moises-laptop.(none)>
2012-02-28 01:24:14 +0100

    Foreign Keys added

--- notesif/studentapp/models.py
+++ notesif/studentapp/models.py
@@ -18,23 +18,23 @@ class Log(models.Model):
         db_table = u'LOG'
 
 class LogEleve(models.Model):
-    user = models.CharField(max_length=60)
+    user = models.ForeignKey('User',max_length=60,db_column='uid')
     date = models.DateTimeField(db_column='Date') # Field name made lowercase.
     ip = models.CharField(max_length=45, db_column='IP') # Field name made lowercase.
     class Meta:
         db_table = u'LOG_eleve'
 
 class Absence(models.Model):
-    ideleve = models.FloatField(db_column='idEleve') # Field name made lowercase.
+    ideleve = models.ForeignKey('Eleve',db_column='idEleve') # Field name made lowercase.
     date = models.DateField(db_column='Date') # Field name made lowercase.
-    idactivite = models.CharField(max_length=24, db_column='idActivite') # Field name made lowercase.
+    idactivite = models.ForeignKey('Activite',max_length=24, db_column='idActivite') # Field name made lowercase.
     class Meta:
         db_table = u'absence'
 
 class Activite(models.Model):
     idactivite = models.CharField(max_length=24, db_column='idActivite') # Field name made lowercase.
-    idmatiere = models.CharField(max_length=18, db_column='idMatiere') # Field name made lowercase.
-    type = models.CharField(max_length=3, db_column='Type') # Field name made lowercase.
+    idmatiere = models.ForeignKey('Matiere',max_length=18, db_column='idMatiere') # Field name made lowercase.
+    type = models.ForeignKey('Typeactivite',max_length=3, db_column='Type') # Field name made lowercase.
     coefficient = models.FloatField(db_column='Coefficient') # Field name made lowercase.
     nom = models.CharField(max_length=768, db_column='Nom') # Field name made lowercase.
     nbhprevu = models.IntegerField(db_column='NbHPrevu') # Field name made lowercase.
@@ -60,8 +60,8 @@ class Anneeuniversitaire(models.Model):
         db_table = u'anneeuniversitaire'
 
 class BilanActivite(models.Model):
-    idanneeuniversitaire = models.IntegerField(db_column='idAnneeUniversitaire') # Field name made lowercase.
-    idactivite = models.CharField(max_length=24, db_column='idActivite') # Field name made lowercase.
+    idanneeuniversitaire = models.ForeignKey('Anneeuniversitaire',db_column='idAnneeUniversitaire') # Field name made lowercase.
+    idactivite = models.ForeignKey('Activite',max_length=24, db_column='idActivite') # Field name made lowercase.
     min = models.FloatField()
     max = models.FloatField()
     moyenne = models.FloatField()
@@ -72,8 +72,8 @@ class BilanActivite(models.Model):
 
 class BilanEleve(models.Model):
     idbilaneleve = models.IntegerField(primary_key=True, db_column='idBilanEleve') # Field name made lowercase.
-    ideleve = models.IntegerField(db_column='idEleve') # Field name made lowercase.
-    idcontratpedagogique = models.IntegerField(db_column='idContratPedagogique') # Field name made lowercase.
+    ideleve = models.ForeignKey('Eleve',db_column='idEleve') # Field name made lowercase.
+    idcontratpedagogique = models.ForeignKey('Contratpedagogique',db_column='idContratPedagogique') # Field name made lowercase.
     moyenne = models.FloatField()
     moyenneds = models.FloatField(db_column='moyenneDS') # Field name made lowercase.
     coefficientds = models.FloatField(db_column='coefficientDS') # Field name made lowercase.
@@ -98,7 +98,7 @@ class BilanEleve(models.Model):
 
 class BilanLaniere(models.Model):
     idbilanlaniere = models.IntegerField(primary_key=True, db_column='idBilanLaniere') # Field name made lowercase.
-    ideleve = models.IntegerField(db_column='idEleve') # Field name made lowercase.
+    ideleve = models.ForeignKey('Eleve',db_column='idEleve') # Field name made lowercase.
     moyenne = models.FloatField()
     moyenneds = models.FloatField(db_column='moyenneDS') # Field name made lowercase.
     coefficientds = models.FloatField(db_column='coefficientDS') # Field name made lowercase.
@@ -106,7 +106,7 @@ class BilanLaniere(models.Model):
     coefficienttp = models.FloatField(db_column='coefficientTP') # Field name made lowercase.
     classement = models.IntegerField()
     nbeleves = models.IntegerField(db_column='nbEleves') # Field name made lowercase.
-    idlaniere = models.CharField(max_length=9, db_column='idLaniere') # Field name made lowercase.
+    idlaniere = models.ForeignKey('Laniere',max_length=9, db_column='idLaniere') # Field name made lowercase.
     qualification = models.CharField(max_length=3)
     rattrapage = models.CharField(max_length=1)
     ectsvalides = models.IntegerField(db_column='ectsValides') # Field name made lowercase.
@@ -115,7 +115,7 @@ class BilanLaniere(models.Model):
 
 class BilanMatiere(models.Model):
     idbilanmatiere = models.IntegerField(primary_key=True, db_column='idBilanMatiere') # Field name made lowercase.
-    ideleve = models.IntegerField(db_column='idEleve') # Field name made lowercase.
+    ideleve = models.ForeignKey('Eleve',db_column='idEleve') # Field name made lowercase.
     moyenne = models.FloatField()
     moyenneds = models.FloatField(db_column='moyenneDS') # Field name made lowercase.
     coefficientds = models.FloatField(db_column='coefficientDS') # Field name made lowercase.
@@ -123,35 +123,36 @@ class BilanMatiere(models.Model):
     coefficienttp = models.FloatField(db_column='coefficientTP') # Field name made lowercase.
     classement = models.IntegerField()
     nbeleves = models.IntegerField(db_column='nbEleves') # Field name made lowercase.
-    idmatiere = models.CharField(max_length=18, db_column='idMatiere') # Field name made lowercase.
+    idmatiere = models.ForeignKey('Matiere',max_length=18, db_column='idMatiere') # Field name made lowercase.
     qualification = models.CharField(max_length=3)
     class Meta:
         db_table = u'bilan_matiere'
 
 class Contenucontratpedagogique(models.Model):
-    idcontratpedagogique = models.FloatField(db_column='idContratPedagogique') # Field name made lowercase.
-    idmatiereyoratooid = models.CharField(max_length=30, db_column='idMatiereYoratooID') # Field name made lowercase.
+    idcontratpedagogique = models.ForeignKey ('Contratpedagogique',db_column='idContratPedagogique') # Field name made lowercase.
+    idmatiereyoratooid = models.ForeignKey('Matiere',max_length=30, db_column='idMatiereYoratooID') # Field name made lowercase.
     coefficient = models.FloatField(db_column='Coefficient') # Field name made lowercase.
     class Meta:
         db_table = u'contenucontratpedagogique'
 
 class Contratpedagogique(models.Model):
     idcontratpedagogique = models.FloatField(primary_key=True, db_column='idContratPedagogique') # Field name made lowercase.
-    idanneeuniversitaire = models.IntegerField(db_column='idAnneeUniversitaire') # Field name made lowercase.
-    idepoque = models.IntegerField(db_column='idEpoque') # Field name made lowercase.
+    idanneeuniversitaire = models.ForeignKey('Anneeuniversitaire',db_column='idAnneeUniversitaire') # Field name made lowercase.
+    idepoque = models.ForeignKey('Epoque',db_column='idEpoque') # Field name made lowercase.
     credits = models.FloatField(db_column='Credits') # Field name made lowercase.
     nom = models.CharField(max_length=60, db_column='Nom') # Field name made lowercase.
     class Meta:
         db_table = u'contratpedagogique'
 
 class CoursEleve(models.Model):
-    ideleve = models.FloatField(db_column='idEleve') # Field name made lowercase.
-    idactivite = models.CharField(max_length=24, db_column='idActivite') # Field name made lowercase.
+    ideleve = models.ForeignKey('Eleve',db_column='idEleve') # Field name made lowercase.
+    idactivite = models.ForeignKey('Activite',max_length=24, db_column='idActivite') # Field name made lowercase.
     class Meta:
         db_table = u'cours_eleve'
 
 class CreditsEtranger(models.Model):
-    ideleve = models.FloatField(primary_key=True, db_column='idEleve') # Field name made lowercase.
+    #ideleve = models.FloatField(primary_key=True, db_column='idEleve') # Field name made lowercase.
+    ideleve = models.ForeignKey('Eleve', db_column='idEleve') # Field name made lowercase.
     credits = models.IntegerField()
     etablissement = models.CharField(max_length=120)
     auteur = models.CharField(max_length=30, db_column='Auteur') # Field name made lowercase.
@@ -161,11 +162,11 @@ class CreditsEtranger(models.Model):
         db_table = u'credits_etranger'
 
 class Dispense(models.Model):
-    ideleve = models.FloatField(db_column='idEleve') # Field name made lowercase.
+    ideleve = models.ForeignKey('Eleve',db_column='idEleve') # Field name made lowercase.
     auteur = models.CharField(max_length=30, db_column='Auteur') # Field name made lowercase.
     date = models.DateTimeField(db_column='Date') # Field name made lowercase.
     raison = models.CharField(max_length=120, db_column='Raison') # Field name made lowercase.
-    idactivite = models.CharField(max_length=24, db_column='idActivite') # Field name made lowercase.
+    idactivite = models.CharField('Activite',max_length=24, db_column='idActivite') # Field name made lowercase.
     ip = models.CharField(max_length=45, db_column='IP') # Field name made lowercase.
     type = models.CharField(max_length=3, db_column='Type') # Field name made lowercase.
     class Meta:
@@ -178,13 +179,13 @@ class Eleve(models.Model):
     binomes1 = models.IntegerField(db_column='Binomes1') # Field name made lowercase.
     binomes2 = models.IntegerField(db_column='Binomes2') # Field name made lowercase.
     binomes3 = models.IntegerField(db_column='Binomes3') # Field name made lowercase.
-    annee = models.IntegerField(db_column='Annee') # Field name made lowercase.
+    annee = models.IntegerField('Anneeuniversitaire',db_column='Annee') # Field name made lowercase.
     groupe = models.IntegerField(db_column='Groupe') # Field name made lowercase.
     echange = models.CharField(max_length=3, db_column='Echange') # Field name made lowercase.
     datenaissance = models.DateField(db_column='DateNaissance') # Field name made lowercase.
     origine = models.CharField(max_length=3, db_column='Origine') # Field name made lowercase.
     mail = models.CharField(max_length=150)
-    type = models.CharField(max_length=3, db_column='Type') # Field name made lowercase.
+    type = models.ForeignKey('Eleve',max_length=3, db_column='Type') # Field name made lowercase.
     tuteur = models.CharField(max_length=6, db_column='Tuteur') # Field name made lowercase.
     class Meta:
         db_table = u'eleve'
@@ -192,14 +193,15 @@ class Eleve(models.Model):
 class Epoque(models.Model):
     idepoque = models.IntegerField(primary_key=True, db_column='idEpoque') # Field name made lowercase.
     nom = models.CharField(max_length=60, db_column='Nom') # Field name made lowercase.
-    annee = models.IntegerField(db_column='Annee') # Field name made lowercase.
+    annee = models.ForeignKey('Anneeuniversitaire',db_column='Annee') # Field name made lowercase.
     semestre = models.IntegerField(db_column='Semestre') # Field name made lowercase.
     class Meta:
         db_table = u'epoque'
 
 class EtudiantContratpedagogique(models.Model):
-    ideleve = models.FloatField(primary_key=True, db_column='idEleve') # Field name made lowercase.
-    idcontratpedagogique = models.FloatField(db_column='idContratPedagogique') # Field name made lowercase.
+   # ideleve = models.FloatField(primary_key=True, db_column='idEleve') # Field name made lowercase.
+    ideleve = models.ForeignKey('Eleve', db_column='idEleve') # Field name made lowercase.
+    idcontratpedagogique = models.ForeignKey('Contratpedagogique',db_column='idContratPedagogique') # Field name made lowercase.
     valide = models.CharField(max_length=1, db_column='Valide') # Field name made lowercase.
     classementvisible = models.IntegerField(db_column='ClassementVisible') # Field name made lowercase.
     class Meta:
@@ -212,13 +214,13 @@ class Laniere(models.Model):
     barretp = models.FloatField(db_column='BarreTP') # Field name made lowercase.
     barregenerale = models.FloatField(db_column='BarreGenerale') # Field name made lowercase.
     nomreseau = models.CharField(max_length=45, db_column='NomReseau') # Field name made lowercase.
-    nomen = models.CharField(max_length=150, db_column='NomEn') # Field name made lowercase.
+    nomen = models.ForeignKey('Profs',max_length=150, db_column='NomEn') # Field name made lowercase.
     class Meta:
         db_table = u'laniere'
 
 class Matiere(models.Model):
     idmatiere = models.CharField(max_length=18, db_column='idMatiere') # Field name made lowercase.
-    idlaniere = models.CharField(max_length=9, db_column='idLaniere') # Field name made lowercase.
+    idlaniere = models.ForeignKey('Laniere',max_length=9, db_column='idLaniere') # Field name made lowercase.
     nom = models.CharField(max_length=210, db_column='Nom') # Field name made lowercase.
     annee = models.IntegerField(db_column='Annee') # Field name made lowercase.
     credits = models.FloatField(db_column='Credits') # Field name made lowercase.
@@ -231,7 +233,7 @@ class MessagesEleve(models.Model):
     idmessage = models.IntegerField(primary_key=True, db_column='IDMessage') # Field name made lowercase.
     message = models.CharField(max_length=765, db_column='Message') # Field name made lowercase.
     date = models.DateField(db_column='Date') # Field name made lowercase.
-    annee = models.CharField(max_length=3, db_column='Annee') # Field name made lowercase.
+    annee = models.ForeignKey('Anneeuniversitaire',max_length=3, db_column='Annee') # Field name made lowercase.
     class Meta:
         db_table = u'messages_eleve'
 
@@ -241,14 +243,14 @@ class News(models.Model):
     link = models.CharField(max_length=765)
     description = models.TextField()
     author = models.CharField(max_length=765)
-    annee = models.IntegerField()
+    annee = models.ForeignKey('Anneeuniversitaire',db_column='Anne')
     class Meta:
         db_table = u'news'
 
 class Note(models.Model):
     idnote = models.IntegerField(primary_key=True, db_column='idNote') # Field name made lowercase.
-    ideleve = models.FloatField(db_column='idEleve') # Field name made lowercase.
-    idactivite = models.CharField(max_length=24, db_column='idActivite') # Field name made lowercase.
+    ideleve = models.ForeignKey('Eleve',db_column='idEleve') # Field name made lowercase.
+    idactivite = models.ForeignKey('Activite',max_length=24, db_column='idActivite') # Field name made lowercase.
     auteur = models.CharField(max_length=30, db_column='Auteur') # Field name made lowercase.
     date = models.DateTimeField(db_column='Date') # Field name made lowercase.
     ip = models.CharField(max_length=45, db_column='IP') # Field name made lowercase.
@@ -261,16 +263,16 @@ class Note(models.Model):
 
 class Plage(models.Model):
     idplage = models.CharField(max_length=24, db_column='idPlage') # Field name made lowercase.
-    annee = models.CharField(max_length=3, db_column='Annee') # Field name made lowercase.
-    idactivite = models.CharField(max_length=24, db_column='idActivite') # Field name made lowercase.
+    annee = models.ForeignKey('Anneeuniversitaire',max_length=3, db_column='Annee') # Field name made lowercase.
+    idactivite = models.ForeignKey('Activite',max_length=24, db_column='idActivite') # Field name made lowercase.
     semaine = models.IntegerField(db_column='Semaine') # Field name made lowercase.
     jour = models.IntegerField(db_column='Jour') # Field name made lowercase.
     class Meta:
         db_table = u'plage'
 
 class Profactivite(models.Model):
-    idprof = models.CharField(max_length=24, db_column='idProf') # Field name made lowercase.
-    idactivite = models.CharField(max_length=24, db_column='idActivite') # Field name made lowercase.
+    idprof = models.ForeignKey('Profs',max_length=24, db_column='idProf') # Field name made lowercase.
+    idactivite = models.ForeignKey('Activite',max_length=24, db_column='idActivite') # Field name made lowercase.
     class Meta:
         db_table = u'profactivite'
 
@@ -285,7 +287,7 @@ class Profs(models.Model):
 
 class ResponsableLaniere(models.Model):
     nomreseau = models.CharField(max_length=45, db_column='NomReseau') # Field name made lowercase.
-    idlaniere = models.CharField(max_length=6, db_column='idLaniere') # Field name made lowercase.
+    idlaniere = models.ForeignKey('Laniere',max_length=6, db_column='idLaniere') # Field name made lowercase.
     annee = models.IntegerField(db_column='Annee') # Field name made lowercase.
     class Meta:
         db_table = u'responsable_laniere'
