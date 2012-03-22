003be9bba9a0e3666074432ea52f72813252e7a3
ychevalier <yann.m.chevalier@gmail.com>
2012-03-22 16:47:10 +0100

    Adding support for LDAP connections.

--- notesif/studentapp/views.py
+++ notesif/studentapp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 import datetime
 
 def home(request):
-    return render(request, "all.html", { "updated_time": datetime.datetime.now() })
+    return render(request, "all.html", { "updated_time": datetime.datetime.now(), "profile": request.user.get_profile() })
